10240abafbdf9cefa7e76863e8e36dd46ade0dcd
aniketk13 <ani.khajanchi257@gmail.com>
2023-03-21 01:27:44 +0530

    pata ni kya horiya hai

--- app/src/main/java/com/teamdefine/farmapp/farmer/bidding/FarmerBiddingAdapter.kt
+++ app/src/main/java/com/teamdefine/farmapp/farmer/bidding/FarmerBiddingAdapter.kt
@@ -3,25 +3,39 @@ package com.teamdefine.farmapp.farmer.bidding
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import com.teamdefine.farmapp.R
 
-class FarmerBiddingAdapter() : RecyclerView.Adapter<FarmerBiddingAdapter.ViewHolder>() {
+class FarmerBiddingAdapter(private val bidList: ArrayList<String>) :
+    RecyclerView.Adapter<FarmerBiddingAdapter.ViewHolder>() {
+    private lateinit var db: FirebaseFirestore
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val bidAmount: TextView = itemView.findViewById(R.id.bidAmount)
+        val nameOfBuyer: TextView = itemView.findViewById(R.id.nameOfBuyer)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        db = FirebaseFirestore.getInstance()
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_farmer_bid, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        TODO("Not yet implemented")
+        val currentItem = bidList[position]
+        db.collection("Bidding").document(currentItem).get().addOnSuccessListener {
+            holder.bidAmount.text = it.get("buyer_bid").toString()
+            db.collection("Buyers").document(it.get("buyer_id").toString()).get()
+                .addOnSuccessListener {
+                    holder.nameOfBuyer.text = it.get("name").toString()
+                }
+        }
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return 2
     }
 }
--- app/src/main/java/com/teamdefine/farmapp/farmer/bidding/FarmerBiddingFragment.kt
+++ app/src/main/java/com/teamdefine/farmapp/farmer/bidding/FarmerBiddingFragment.kt
@@ -1,6 +1,7 @@
 package com.teamdefine.farmapp.farmer.bidding
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,32 +10,60 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.teamdefine.farmapp.databinding.FragmentFarmerBiddingBinding
 
 class FarmerBiddingFragment : Fragment() {
     private val viewModel: FarmerBiddingViewModel by viewModels()
     private lateinit var binding: FragmentFarmerBiddingBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
     private val args: FarmerBiddingFragmentArgs by navArgs()
     private var adapter: RecyclerView.Adapter<FarmerBiddingAdapter.ViewHolder>? = null //adapter
+    private var bidList: ArrayList<String> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = FragmentFarmerBiddingBinding.inflate(inflater, container, false).also {
         binding = it
+        db = FirebaseFirestore.getInstance()
         firebaseAuth = FirebaseAuth.getInstance()
     }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val cropId = args.itemId
+        db.collection("Crops").document(cropId).get().addOnSuccessListener { it ->
+            val options: RequestOptions = RequestOptions()
+                .centerCrop()
+            Glide.with(requireContext()).load(it.get("image").toString()).apply(options)
+                .into(binding.cropIv)
 
+            binding.cropNameTv.text = it.get("itemName").toString()
+            binding.priceOfCrop.text = it.get("itemPrice").toString() + "/क्विंटल"
+
+            val farmerId = it.get("farmerId")
+            db.collection("Farmers").document(farmerId.toString()).get().addOnSuccessListener {
+                binding.farmerNameTv.text = it.get("name").toString()
+                db.collection("Bidding").whereEqualTo("item_id", it.get("itemId")).get()
+                    .addOnSuccessListener {
+                        for (i in it) {
+                            Log.i("test", i.data.toString())
+                            bidList.add(i.data.toString())
+                        }
+                    }
+            }
+
+        }
         initViews()
     }
 
     private fun initViews() {
-        adapter = FarmerBiddingAdapter()
+        adapter = FarmerBiddingAdapter(bidList)
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(activity)
     }
