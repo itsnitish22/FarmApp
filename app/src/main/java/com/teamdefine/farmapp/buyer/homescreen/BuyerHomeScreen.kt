a89daef7e0241350875e1c264f01b4ef6816d1b4
aniketk13 <ani.khajanchi257@gmail.com>
2023-03-20 11:20:10 +0530

    jaldi kaam khtm kr

--- app/src/main/java/com/teamdefine/farmapp/buyer/homescreen/BuyerHomeScreen.kt
+++ app/src/main/java/com/teamdefine/farmapp/buyer/homescreen/BuyerHomeScreen.kt
@@ -1,32 +1,48 @@
 package com.teamdefine.farmapp.buyer.homescreen
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.teamdefine.farmapp.R
+import com.teamdefine.farmapp.databinding.FragmentBuyerHomeScreenBinding
+import com.teamdefine.farmapp.databinding.FragmentFarmerRegisterBinding
 
 class BuyerHomeScreen : Fragment() {
 
-    companion object {
-        fun newInstance() = BuyerHomeScreen()
-    }
-
     private lateinit var viewModel: BuyerHomeScreenViewModel
+    private lateinit var binding:FragmentBuyerHomeScreenBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var cropsData: ArrayList<Any> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_buyer_home_screen, container, false)
+        binding = FragmentBuyerHomeScreenBinding.inflate(inflater, container, false)
+        firebaseAuth = FirebaseAuth.getInstance()
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(BuyerHomeScreenViewModel::class.java)
-        // TODO: Use the ViewModel
+        val db = FirebaseFirestore.getInstance()
+        db.collection("Crops").get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    cropsData.add(document.data)
+                    Log.i("helloabc", "${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.i("helloabc", "Error getting documents: ", exception)
+            }
     }
 
 }
